5f872903033e8624b9fbfecfa750ba74325d47bc
Jinke.Li <1359518268@qq.com>
2019-07-26 14:50:43 +0800

    feat: upgrade test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,30 +1,34 @@
 import React from 'react'
 import { shallow, mount } from 'enzyme'
-import { Form } from 'antd'
+import { Form, Input } from 'antd'
 
 import withAntdFormHasError from '../src'
 
 describe('antd form create hoc test', () => {
-  let Component
-  beforeEach(() => [
-    (Component = ({ form: { getFieldDecorator } }) => (
-      <Form>
-        <Form.Item>
-          {getFieldDecorator('username', {
-            rules: [{ required: true, message: 'Please input your username!' }]
-          })(<Input placeholder="Username" />)}
-        </Form.Item>
-        <Form.Item>
-          {getFieldDecorator('password', {
-            rules: [{ required: false, message: 'Please input your Password!' }]
-          })(<Input type="password" placeholder="Password" />)}
-        </Form.Item>
-      </Form>
-    ))
-  ])
   it('should get hasError from this.props', () => {
-    const App = withAntdFormHasError()(Component)
-    const wrapper = mount(<App />)
+    @Form.create()
+    @withAntdFormHasError()
+    class Test extends React.Component {
+      render() {
+        const { getFieldDecorator } = this.props.form
+        return (
+          <Form>
+            <Form.Item>
+              {getFieldDecorator('username', {
+                rules: [{ required: true, message: 'Please input your username!' }]
+              })(<Input placeholder="Username" />)}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('password', {
+                rules: [{ required: true, message: 'Please input your Password!' }]
+              })(<Input type="password" placeholder="Password" />)}
+            </Form.Item>
+          </Form>
+        )
+      }
+    }
+    const wrapper = mount(shallow(<Test />).get(0))
+    console.log(wrapper.props())
     expect(wrapper.props().hasError).toEqual(true)
   })
 })
